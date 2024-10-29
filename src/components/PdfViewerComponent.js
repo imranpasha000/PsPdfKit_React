e0c2d5551b161ec5438bd045d8b1384a0781307e
Mohammad Imran <pashaturki19081@gmail.com>
2024-10-29 17:06:11 +0530

    Update PdfViewerComponent.js
    add the toolitrms

--- src/components/PdfViewerComponent.js
+++ src/components/PdfViewerComponent.js
@@ -9,13 +9,56 @@ export default function PdfViewerComponent(props) {
 
     (async function () {
       PSPDFKit = await import("pspdfkit");
-      await PSPDFKit.load({
-        // Container where PSPDFKit should be mounted.
+      const instance = await PSPDFKit.load({
         container,
-        // The document to open.
         document: props.document,
-        // Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
         baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
+        toolbarItems: [
+          { type: "sidebar-thumbnails" },
+          { type: "sidebar-document-outline" },
+          { type: "sidebar-annotations" },
+          { type: "sidebar-bookmarks" },
+          { type: "sidebar-signatures" },
+          { type: "sidebar-layers" },
+          { type: "pager" },
+          { type: "pan" },
+          { type: "zoom-out" },
+          { type: "zoom-in" },
+          { type: "zoom-mode" },
+          { type: "spacer" },
+          { type: "annotate" },
+          { type: "ink" },
+          { type: "highlighter" },
+          { type: "text-highlighter" },
+          { type: "ink-eraser" },
+          { type: "signature" },
+          { type: "image" },
+          { type: "stamp" },
+          { type: "note" },
+          { type: "text" },
+          { type: "line" },
+          { type: "arrow" },
+          { type: "rectangle" },
+          { type: "cloudy-rectangle" },
+          { type: "dashed-rectangle" },
+          { type: "ellipse" },
+          { type: "cloudy-ellipse" },
+          { type: "dashed-ellipse" },
+          { type: "polygon" },
+          { type: "cloudy-polygon" },
+          { type: "dashed-polygon" },
+          { type: "polyline" },
+          { type: "print" },
+          { type: "document-editor" },
+          { type: "document-crop" },
+          { type: "search" },
+          { type: "export-pdf" },
+          { type: "debug" },
+          { type: "content-editor" },
+          { type: "link" },
+          { type: "multi-annotations-selection" },
+          { type: "callout" },
+        ],
       });
     })();
 
